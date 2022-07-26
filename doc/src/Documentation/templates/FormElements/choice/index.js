e8e46d2e2377d5ab9f7378353d814761476c959f
AnupChakravarthi <nellutlalnrao@gmail.com>
2022-07-26 22:12:38 +0530

    Updated Choice in FormElements

--- doc/src/Documentation/templates/FormElements/choice/index.js
+++ doc/src/Documentation/templates/FormElements/choice/index.js
@@ -2,5 +2,13 @@ import React from 'react';
 import { Choice } from '@LibFormElements/Choice/index.js';
 
 export const ChoicePage = () =>{
- return (<Choice />);
+ return (<>
+ <Choice id="numbers" name="numbers" label="One" value="1" type="radio" disabled={false} />
+ <Choice id="numbers" name="numbers" label="Two" value="2" type="radio" disabled={false} />
+ <Choice id="numbers" name="numbers" label="Three" value="3" type="radio" disabled={false} />
+
+ <Choice id="numbers" name="numbers" label="One" value="1" type="checkbox" disabled={false} />
+ <Choice id="numbers" name="numbers" label="Two" value="2" type="checkbox" disabled={false} />
+ <Choice id="numbers" name="numbers" label="Three" value="3" type="checkbox" disabled={false} />
+ </>);
 };
--- doc/src/FormElements/Choice/index.js
+++ doc/src/FormElements/Choice/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import './index.css';
 
-export const Choice = () =>{
- return <div>Write Choice</div>;
+export const Choice = ({ id, name, label, value, type, checked, disabled }) =>{
+ return (<div class="form-check form-switch">
+ <input class="form-check-input" type={type} name={name} value={value} id={id} checked={checked} disabled={disabled} />
+ <label class="form-check-label" for={id}>{label}</label>
+</div>);
 };
