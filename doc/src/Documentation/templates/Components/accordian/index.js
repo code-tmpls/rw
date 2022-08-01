bc860bed20851155c6dcb6fed8786acc3cb4931c
AnupChakravarthi <nellutlalnrao@gmail.com>
2022-08-01 23:39:21 +0530

    Updated Accordian Documentation

--- doc/src/Documentation/templates/Components/accordian/index.js
+++ doc/src/Documentation/templates/Components/accordian/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SimpleHeader, Grid, Order, Highlight } from 'react-webpack-lib';
+import { SimpleHeader, Grid, Order, Card, Highlight } from 'react-webpack-lib';
 import SampleCode, { SampleNote1, SampleNote2 } from './components/SampleCode.js';
 import { default as SampleCodeJS  } from '!!raw-loader!./components/SampleCode.js';
 
@@ -7,19 +7,25 @@ const AccordianNotes = ()=>{
 console.log(SampleCodeJS.toString());
  const Note1 = () =>{
   return (<>
-  <div className="mbot10p"><b>Simple Accordian that allows Toggle on Collapse:</b></div>
-  <SampleNote1 />
-  <div className="mtop10p mbot10p"><b>Sample Code:</b></div>
+  <div className="mtop10p mbot10p"><b>Simple Accordian that allows Toggle on Collapse:</b></div>
+  <Card component={<>
+  <div className="mbot10p"><b>Sample Code:</b></div>
   <Highlight content={SampleCodeJS.toString()} lang="javascript" lines={['5T9']} />
+  <div className="mtop10p mbot10p"><b>Output:</b></div>
+  <SampleNote1 />
+  </>} />
   </>);
  };
 
  const Note2 = () =>{
   return (<>
-  <div className="mbot10p"><b>Accordian with default Open:</b></div>
-  <SampleNote2 />
-  <div className="mtop10p mbot10p"><b>Sample Code:</b></div>
+  <div className="mtop10p mbot10p"><b>Accordian with default Open:</b></div>
+  <Card component={<>
+  <div className="mbot10p"><b>Sample Code:</b></div>
   <Highlight content={SampleCodeJS.toString()} lang="javascript" lines={['15T20']} />
+  <div className="mtop10p mbot10p"><b>Output:</b></div>
+  <SampleNote2 />
+  </>} />
   </>);
  };
 
