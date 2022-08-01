996b8350887c1e75da0f7f71bd1851e3c6451c17
AdithyaKankipati <adithyakankipati28@gmail.com>
2022-08-01 19:57:07 +0530

    Added Range, TextArea, TextBox

--- doc/src/Library/FormElements/Range/index.js
+++ doc/src/Library/FormElements/Range/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
 export const Range = () =>{
- return <div>Write Range</div>;
+ return(  <div class="">
+    <label for="customRange" classname="form-label">Custom range</label>
+ <input type="range" class="form-range" id="customRange" name="points"/> 
+ </div>);
 };
