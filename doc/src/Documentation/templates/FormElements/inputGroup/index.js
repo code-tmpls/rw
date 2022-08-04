ef16d7651e921900ef840832e02649e17f8785b5
AnupChakravarthi <nellutlalnrao@gmail.com>
2022-08-04 16:44:35 +0530

    Updated Input Group Documentation

--- doc/src/Documentation/templates/FormElements/inputGroup/index.js
+++ doc/src/Documentation/templates/FormElements/inputGroup/index.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import { Grid, SimpleHeader, InputGroup } from 'react-webpack-lib';
+import { SimpleHeader, Grid, Order, Card, Highlight, Colors, InputGroup } from 'react-webpack-lib';
+import SampleCode from './components/SampleCode.js';
+import { default as SampleCodeJS  } from '!!raw-loader!./components/SampleCode.js';
 
-const inputGroupData = [{ elementType:"button", type:"primary", label:"Primary Button"  },
-{ elementType:"checkbox", id:"element1", name:"element3", value:"hello1" },
-{ elementType:"radio", id:"element2", name:"element4", value:"hello2" },
-{ elementType:"switch", id:"element5", name:"element6", value:"hello3" },
-{ elementType:"span", label:"@" },
-{ elementType:"datepicker",  id:"lastName", name:"lastName" },
-{ elementType:"textbox", id:"firstName", name:"firstName", placeholder:"Enter your FirstName" },
-{ elementType:"select", label:"Select Theme", options:[{ id:'light', label:'Light Theme', value:'Light' },
-          { id:'dark', label:'Dark Theme', value:'Dark' }],
-className:"navbar-layout",
-width:"130",
-fontSize:"12" }
-];
+const InputGroupNotes = ()=>{
+   const Note1 = () =>{
+      return (<>
+       <div className="mtop10p mbot10p"><b>Basic Input Group:</b></div>
+       <Card backgroundColor={Colors.grey} component={<>
+  <div className="mbot10p"><b>Sample Code:</b></div>
+  <Highlight content={SampleCodeJS.toString()} lang="javascript" lines={['4T15']} />
+  <Highlight content={SampleCodeJS.toString()} lang="javascript" lines={['18']} />
+  <div className="mtop10p mbot10p"><b>Output:</b></div>
+  <SampleCode />
+  </>} />
+      </>);
+   };
+
+   return (<Order data={[<Note1 />]} />);
+}; //
 
 export const InputGroupPage = () =>{
  return (<><SimpleHeader title="InputGroup" />
  <Grid data={[
-    [{ sm:12, md:'', lg:'', xl:'', xxl:'', component:<InputGroup data={inputGroupData} /> }]
+    [{ sm:12, md:'', lg:'', xl:'', xxl:'', component:<InputGroupNotes /> }]
  ]} />
  </>);
 };
