0834365fd4e09066905af1f4aaf6b66509c1da16
AnupChakravarthi <nellutlalnrao@gmail.com>
2022-08-05 00:09:46 +0530

    Updated Alerts Library and Documentation

--- doc/src/Documentation/templates/Components/alert/components/SampleCode.js
+++ doc/src/Documentation/templates/Components/alert/components/SampleCode.js
@@ -2,103 +2,103 @@ import React from "react";
 import { Alert } from 'react-webpack-lib';
 
 export const SampleNote1 = () =>{
- return (<>
+ return (<div>
   <Alert type="primary" show={true} heading={<div>Alert Heading</div>} body={<div>Alert Body and Its Content</div>} />
- </>);
+ </div>);
 };
 
 export const SampleNote2 = () =>{
- return (<>
-  <Alert type="secondary" show={true} heading={<>Alert Heading</>} body={<>Alert Body and Its Content</>} />
- </>);
+ return (<div>
+  <Alert type="secondary" show={true} heading={<div>Alert Heading</div>} body={<div>Alert Body and Its Content</div>} />
+ </div>);
 };
 
 export const SampleNote3 = () =>{
- return (<>
-  <Alert type="success" show={true} heading={<>Alert Heading</>} body={<>Alert Body and Its Content</>} />
- </>);
+ return (<div>
+  <Alert type="success" show={true} heading={<div>Alert Heading</div>} body={<div>Alert Body and Its Content</div>} />
+ </div>);
 };
 
 export const SampleNote4 = () =>{
- return (<>
-  <Alert type="danger" show={true} heading={<>Alert Heading</>} body={<>Alert Body and Its Content</>} />
- </>);
+ return (<div>
+  <Alert type="danger" show={true} heading={<div>Alert Heading</div>} body={<div>Alert Body and Its Content</div>} />
+ </div>);
 };
 
 export const SampleNote5 = () =>{
- return (<>
-  <Alert type="warning" show={true} heading={<>Alert Heading</>} body={<>Alert Body and Its Content</>} />
- </>);
+ return (<div>
+  <Alert type="warning" show={true} heading={<div>Alert Heading</div>} body={<div>Alert Body and Its Content</div>} />
+ </div>);
 };
 
 export const SampleNote6 = () =>{
- return (<>
-  <Alert type="info" show={true} heading={<>Alert Heading</>} body={<>Alert Body and Its Content</>} />
- </>);
+ return (<div>
+  <Alert type="info" show={true} heading={<div>Alert Heading</div>} body={<div>Alert Body and Its Content</div>} />
+ </div>);
 };
 
 export const SampleNote7 = () =>{
- return (<>
-  <Alert type="dark" show={true} heading={<>Alert Heading</>} body={<>Alert Body and Its Content</>} />
- </>);
+ return (<div>
+  <Alert type="dark" show={true} heading={<div>Alert Heading</div>} body={<div>Alert Body and Its Content</div>} />
+ </div>);
 };
 
 export const SampleNote8 = () =>{
- return (<>
-  <Alert type="light" show={true} heading={<>Alert Heading</>} body={<>Alert Body and Its Content</>} />
- </>);
+ return (<div>
+  <Alert type="light" show={true} heading={<div>Alert Heading</div>} body={<div>Alert Body and Its Content</div>} />
+ </div>);
 };
 
 export const SampleNote9 = () =>{
- return (<>
+ return (<div>
   <Alert type="primary" show={true} body={<div>Alert Body and Its Content</div>} />
- </>);
+ </div>);
 };
    
 export const SampleNote10 = () =>{
- return (<>
-  <Alert type="secondary" show={true} body={<>Alert Body and Its Content</>} />
- </>);
+ return (<div>
+  <Alert type="secondary" show={true} body={<div>Alert Body and Its Content</div>} />
+ </div>);
 };
    
 export const SampleNote11 = () =>{
- return (<>
-  <Alert type="success" show={true} body={<>Alert Body and Its Content</>} />
- </>);
+ return (<div>
+  <Alert type="success" show={true} body={<div>Alert Body and Its Content</div>} />
+ </div>);
 };
    
 export const SampleNote12 = () =>{
- return (<>
-  <Alert type="danger" show={true} body={<>Alert Body and Its Content</>} />
- </>);
+ return (<div>
+  <Alert type="danger" show={true} body={<div>Alert Body and Its Content</div>} />
+ </div>);
 };
    
 export const SampleNote13 = () =>{
- return (<>
-  <Alert type="warning" show={true} body={<>Alert Body and Its Content</>} />
- </>);
+ return (<div>
+  <Alert type="warning" show={true} body={<div>Alert Body and Its Content</div>} />
+ </div>);
 };
    
 export const SampleNote14 = () =>{
- return (<>
-  <Alert type="info" show={true} body={<>Alert Body and Its Content</>} />
- </>);
+ return (<div>
+  <Alert type="info" show={true} body={<div>Alert Body and Its Content</div>} />
+ </div>);
 };
    
 export const SampleNote15 = () =>{
- return (<>
-  <Alert type="dark" show={true} body={<>Alert Body and Its Content</>} />
- </>);
+ return (<div>
+  <Alert type="dark" show={true} body={<div>Alert Body and Its Content</div>} />
+ </div>);
 };
    
 export const SampleNote16 = () =>{
- return (<>
-  <Alert type="light" show={true} body={<>Alert Body and Its Content</>} />
- </>);
+ return (<div>
+  <Alert type="light" show={true} body={<div>Alert Body and Its Content</div>} />
+ </div>);
 };
 
 const SampleCode = () =>{
- return (<></>);
+ return (<div></div>);
 };
 
 export default SampleCode;
