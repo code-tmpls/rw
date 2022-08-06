a3401b9d47955075346c0dfec4538407d100109e
AnupChakravarthi <nellutlalnrao@gmail.com>
2022-08-06 11:48:54 +0530

    Updated Breadcrumb in the Library and Documentation

---
+++ doc/src/Documentation/templates/Components/breadcrumb/components/SampleCode.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Breadcrumb, Colors } from 'react-webpack-lib';
+
+export const SampleNote1 = () =>{
+ return (<>
+ <Breadcrumb backgroundColor={Colors.light} />
+ </>);
+};
+
+const SampleCode = () =>{
+ return (<></>);
+};
+  
+export default SampleCode;
--- doc/src/Library/Components/Breadcrumb/index.js
+++ doc/src/Library/Components/Breadcrumb/index.js
@@ -1,5 +1,11 @@
 import React from "react";
 
-export const Breadcrumb = ()=>{
- return (<div>Breadcrumb</div>);
+export const Breadcrumb = ({ backgroundColor })=>{
+ return (<div><nav aria-label="breadcrumb" style={{ padding:'8px', border:'1px solid #ccc', backgroundColor: backgroundColor, borderRadius:'5px' }}>
+ <ol class="breadcrumb" style={{ marginBottom:'0px' }}>
+   <li class="breadcrumb-item"><a href="#">Home</a></li>
+   <li class="breadcrumb-item"><a href="#">Library</a></li>
+   <li class="breadcrumb-item active" aria-current="page">Data</li>
+ </ol>
+</nav></div>);
 };
