5843acc00be823222fbc7bcc9e2ad10036e0ddae
AnupChakravarthi <nellutlalnrao@gmail.com>
2022-08-06 17:43:48 +0530

    Updated Card Documentation

--- doc/src/Documentation/templates/Components/card/index.js
+++ doc/src/Documentation/templates/Components/card/index.js
@@ -1,6 +1,30 @@
 import React from 'react';
-import { SimpleHeader } from 'react-webpack-lib';
+import { SimpleHeader, Grid, Order, Card, Highlight, Colors } from 'react-webpack-lib';
+import { SampleNote1 } from './components/SampleCode.js';
+import { default as SampleCodeJS  } from '!!raw-loader!./components/SampleCode.js';
+
+const CardNotes= ()=>{
+ const Note1 = ()=>{
+  return (<>
+    <div className="mtop10p mbot10p"><b>Simple Card:</b></div>
+        <Card backgroundColor={Colors.grey} component={<>
+        <div className="mbot10p"><b>Sample Code:</b></div>
+        <Highlight content={SampleCodeJS.toString()} lang="javascript" lines={['1']} />
+        <Highlight content={SampleCodeJS.toString()} lang="javascript" lines={['5T7']} />
+        <Highlight content={SampleCodeJS.toString()} lang="javascript" lines={['10']} />
+        <div className="mtop10p mbot10p"><b>Output:</b></div>
+        <SampleNote1 />
+        </>} />
+  </>);
+ };
+ return (<Order data={[<Note1 />]} />);
+};
 
 export const CardPage = () =>{
- return (<><SimpleHeader title="Card" /></>);
+ return (<>
+ <SimpleHeader title="Card" />
+ <Grid data={[
+    [{ sm:12, md:'', lg:'', xl:'', xxl:'', component:<CardNotes /> }]
+  ]} />
+ </>);
 };
