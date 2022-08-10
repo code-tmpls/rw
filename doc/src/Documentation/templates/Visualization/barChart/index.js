fd6b125bbb06d891de68559c94a2ef32b13c4c3e
AnupChakravarthi <nellutlalnrao@gmail.com>
2022-08-10 19:19:08 +0530

    Initialized Bar charts in Library and Documentation

--- doc/src/Documentation/templates/Visualization/barChart/index.js
+++ doc/src/Documentation/templates/Visualization/barChart/index.js
@@ -1,6 +1,158 @@
 import React from 'react';
-import { SimpleHeader } from 'react-webpack-lib';
+import { SimpleHeader, Grid, Order, Card, Highlight, Colors } from 'react-webpack-lib';
+import { SampleNote1, SampleNote2, SampleNote3, SampleNote4, SampleNote5 } from './components/SampleCode.js';
+import { default as SampleCodeJS  } from '!!raw-loader!./components/SampleCode.js';
+
+const BarChartNotes = ()=>{
+    const Note1 = () =>{
+      const NoteChart = ()=>{
+        return (<>
+         <SampleNote1 />
+        </>);
+      };
+  
+      const NoteCode = ()=>{
+        return (<>
+          <div className="mbot10p"><b>Sample Code:</b></div>
+          <Highlight content={SampleCodeJS.toString()} lang="javascript" lines={['0','1']} />
+          <Highlight content={SampleCodeJS.toString()} lang="javascript" lines={['5T11']} />
+          <Highlight content={SampleCodeJS.toString()} lang="html" lines={['14']} />
+        </>);
+      };
+  
+      return (<>
+      <div className="mtop10p mbot10p"><b>Simple Bar Chart:</b></div>
+      <Card backgroundColor={Colors.grey} component={<>
+        <Grid data={[
+          [{ sm:5, md:'', lg:'', xl:'', xxl:'', component:<NoteChart /> },
+          { sm:7, md:'', lg:'', xl:'', xxl:'', component:<NoteCode /> }]
+        ]} />
+      </>} />
+      
+      </>);
+    };
+  
+    const Note2 = () =>{
+      const NoteChart = ()=>{
+        return (<>
+         <SampleNote2 />
+        </>);
+      };
+  
+      const NoteCode = ()=>{
+        return (<>
+          <div className="mbot10p"><b>Sample Code:</b></div>
+          <Highlight content={SampleCodeJS.toString()} lang="javascript" lines={['0','1']} />
+          <Highlight content={SampleCodeJS.toString()} lang="javascript" lines={['21T27']} />
+          <Highlight content={SampleCodeJS.toString()} lang="html" lines={['30']} />
+        </>);
+      };
+  
+      return (<>
+      <div className="mtop10p mbot10p"><b>Stacked Bar Chart:</b></div>
+      <Card backgroundColor={Colors.grey} component={<>
+        <Grid data={[
+          [{ sm:5, md:'', lg:'', xl:'', xxl:'', component:<NoteChart /> },
+          { sm:7, md:'', lg:'', xl:'', xxl:'', component:<NoteCode /> }]
+        ]} />
+      </>} />
+      
+      </>);
+    };
+  
+    const Note3 = () =>{
+      const NoteChart = ()=>{
+        return (<>
+         <SampleNote3 />
+        </>);
+      };
+  
+      const NoteCode = ()=>{
+        return (<>
+          <div className="mbot10p"><b>Sample Code:</b></div>
+          <Highlight content={SampleCodeJS.toString()} lang="javascript" lines={['0','1']} />
+          <Highlight content={SampleCodeJS.toString()} lang="javascript" lines={['37T43']} />
+          <Highlight content={SampleCodeJS.toString()} lang="javascript" lines={['45T47']} />
+          <Highlight content={SampleCodeJS.toString()} lang="html" lines={['50']} />
+        </>);
+      };
+  
+      return (<>
+      <div className="mtop10p mbot10p"><b>Mixed Bar Chart (Combination of Simple and Stacked):</b></div>
+      <Card backgroundColor={Colors.grey} component={<>
+        <Grid data={[
+          [{ sm:5, md:'', lg:'', xl:'', xxl:'', component:<NoteChart /> },
+          { sm:7, md:'', lg:'', xl:'', xxl:'', component:<NoteCode /> }]
+        ]} />
+      </>} />
+      
+      </>);
+    };
+
+    const Note4 = () =>{
+        const NoteChart = ()=>{
+          return (<>
+           <SampleNote4 />
+          </>);
+        };
+    
+        const NoteCode = ()=>{
+          return (<>
+            <div className="mbot10p"><b>Sample Code:</b></div>
+            <Highlight content={SampleCodeJS.toString()} lang="javascript" lines={['0','1']} />
+            <Highlight content={SampleCodeJS.toString()} lang="javascript" lines={['57T63']} />
+            <Highlight content={SampleCodeJS.toString()} lang="html" lines={['66']} />
+          </>);
+        };
+    
+        return (<>
+        <div className="mtop10p mbot10p"><b>Positive and Negative Bar Chart:</b></div>
+        <Card backgroundColor={Colors.grey} component={<>
+          <Grid data={[
+            [{ sm:5, md:'', lg:'', xl:'', xxl:'', component:<NoteChart /> },
+            { sm:7, md:'', lg:'', xl:'', xxl:'', component:<NoteCode /> }]
+          ]} />
+        </>} />
+        
+        </>);
+      };
+
+      const Note5 = () =>{
+        const NoteChart = ()=>{
+          return (<>
+           <SampleNote5 />
+          </>);
+        };
+    
+        const NoteCode = ()=>{
+          return (<>
+            <div className="mbot10p"><b>Sample Code:</b></div>
+            <Highlight content={SampleCodeJS.toString()} lang="javascript" lines={['0','1']} />
+            <Highlight content={SampleCodeJS.toString()} lang="javascript" lines={['72T110']} />
+            <Highlight content={SampleCodeJS.toString()} lang="html" lines={['113']} />
+          </>);
+        };
+    
+        return (<>
+        <div className="mtop10p mbot10p"><b>Brush Bar Chart:</b></div>
+        <Card backgroundColor={Colors.grey} component={<>
+          <Grid data={[
+            [{ sm:5, md:'', lg:'', xl:'', xxl:'', component:<NoteChart /> },
+            { sm:7, md:'', lg:'', xl:'', xxl:'', component:<NoteCode /> }]
+          ]} />
+        </>} />
+        
+        </>);
+      };
+  
+    return (<Order data={[<Note1 />, <Note2 />, <Note3 />, <Note4 />,<Note5 />]} />);
+};
 
 export const BarChartPage = () =>{
- return (<><SimpleHeader title="BarChart" /></>);
+    return (<>
+        <SimpleHeader title="BarChart" />
+        <Grid data={[
+          [{ sm:12, md:'', lg:'', xl:'', xxl:'', component:<BarChartNotes /> }]
+        ]} />
+       </>);
 };
--- doc/src/Library/Visualization/BarChart/index.js
+++ doc/src/Library/Visualization/BarChart/index.js
@@ -1,5 +1,129 @@
 import React from "react";
+import { BarChart as BarChartReChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, 
+   ReferenceLine, Brush } from 'recharts';
+import './../index.css';
 
-export const BarChart = () =>{
- return (<div>BarChart</div>);
+export const BarChart = ({ type, height, data, stacks }) =>{
+ const SimpleBarChart = ()=>{
+  return (<ResponsiveContainer width="100%" height={height}>
+   <BarChartReChart width={500} height={height} data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }} >
+    <XAxis dataKey="name" />
+    <YAxis />
+    <CartesianGrid strokeDasharray="3 3" />
+    <Tooltip />
+    <Legend />
+    <Bar dataKey="pv" fill="#8884d8" />
+    <Bar dataKey="uv" fill="#82ca9d" />
+  </BarChartReChart>
+  </ResponsiveContainer>);
+ };
+ 
+ const StackedBarChart = ()=>{
+  return (<ResponsiveContainer width="100%" height={height}>
+   <BarChartReChart width={500} height={height} data={data} margin={{ top: 20, right: 30, left: 20, bottom: 5 }} >
+    <CartesianGrid strokeDasharray="3 3" />
+    <XAxis dataKey="name" />
+    <YAxis />
+    <Tooltip />
+    <Legend />
+    <Bar dataKey="pv" stackId="a" fill="#8884d8" />
+    <Bar dataKey="uv" stackId="a" fill="#82ca9d" />
+  </BarChartReChart>
+  </ResponsiveContainer>);
+ };
+
+ const MixedBarChart = () =>{
+  return (<ResponsiveContainer width="100%" height={height}>
+  <BarChartReChart
+   width={500}
+   height={height}
+   data={data}
+   margin={{
+     top: 20,
+     right: 30,
+     left: 20,
+     bottom: 5
+   }}
+ >
+   <CartesianGrid strokeDasharray="3 3" />
+   <XAxis dataKey="name" />
+   <YAxis />
+   <Tooltip />
+   <Legend />
+   {stacks.map(d=>{
+      return (d?.units.map(u=>{
+         return (<Bar dataKey={u.dataKey} stackId={d.id} fill={u.color} />);
+      }))
+   })}
+ </BarChartReChart>
+ </ResponsiveContainer>);
+ };
+
+ const PosNegBarCharts = ()=>{
+  return (<>
+  <ResponsiveContainer width="100%" height={height}>
+        <BarChartReChart
+          width={500}
+          height={height}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <ReferenceLine y={0} stroke="#000" />
+          <Bar dataKey="pv" fill="#8884d8" />
+          <Bar dataKey="uv" fill="#82ca9d" />
+        </BarChartReChart>
+      </ResponsiveContainer>
+  </>);
+ };
+
+ const BrushBarChart = () =>{
+  return (<>
+  <ResponsiveContainer width="100%" height={height}>
+        <BarChartReChart width={500}
+      height={height}
+      data={data}
+      margin={{
+        top: 5,
+        right: 30,
+        left: 20,
+        bottom: 5
+      }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+      <Legend verticalAlign="top" wrapperStyle={{ lineHeight: "40px" }} />
+      <ReferenceLine y={0} stroke="#000" />
+      <Brush dataKey="name" height={30} stroke="#8884d8" />
+      <Bar dataKey="pv" fill="#8884d8" />
+      <Bar dataKey="uv" fill="#82ca9d" />
+         
+   </BarChartReChart>
+   </ResponsiveContainer>
+  </>);
+ };
+
+ if(type==="simple") {
+    return <SimpleBarChart />
+ } else if(type==="stacked") {
+    return <StackedBarChart />
+ } else if(type==='mixed'){
+    return <MixedBarChart />
+ } else if(type==='positiveNegative'){
+    return <PosNegBarCharts />
+ } else if(type==='brush'){
+    return <BrushBarChart />
+ }
+
 };
