d614131ee93f488257dcd0e188436b8a2527e422
AnupChakravarthi <nellutlalnrao@gmail.com>
2022-08-10 15:47:39 +0530

    Updated Range Slider

--- doc/src/Library/FormElements/Range/index.js
+++ doc/src/Library/FormElements/Range/index.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const Range = () =>{
- return(  <div className="">
-    <label for="customRange" classNamename="form-label">Custom range</label>
- <input type="range" className="form-range" id="customRange" name="points"/> 
- </div>);
+ const [ value, setValue ] = useState(30);
+ return(<>
+    <label className="form-label">Custom range</label>
+ <div className="row">
+  <div className="col-10">
+  <input type="range" className="form-range" id="customRange" name="points" value={value} 
+    onChange={(e)=>setValue(e.target.value)} />
+  </div>
+  <div align="center" className="col-2">
+  <span>{value}</span>
+  </div>
+ </div>
+ 
+ 
+ </>);
 };
