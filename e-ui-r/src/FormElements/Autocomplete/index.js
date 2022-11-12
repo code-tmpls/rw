85b266e99254977a44dea1e1847a9cae876c74d4
AnupChakravarthi <nellutlalnrao@gmail.com>
2022-11-12 22:31:52 +0530

    Updated Autocomplete Validation logic

--- e-ui-r/src/FormElements/Autocomplete/index.js
+++ e-ui-r/src/FormElements/Autocomplete/index.js
@@ -1,65 +1,87 @@
 import React, { useState, useEffect } from "react";
 import HtmlToReactParser from 'html-to-react';
-import { FilterArray } from "e-ui-react";
+import { FilterArray, FormInputValidate, KeyPressValidate } from "e-ui-react";
 import './index.css';
 
 // const data = ["Action","Another Action","Something else here"];
 
-export const Autocomplete = ({ name, label, placeholder, value, autoCompleteData, formContext, validation }) =>{
+export const Autocomplete = ({ name, label, placeholder, value, autoCompleteData, formContext, validation }) => {
 
- const [ filteredData, setFilteredData ] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
 
- const [ show, setShow ] = useState(false);
- const [ autoCompleteValue, setAutoCompleteValue ] = useState((value===undefined)?'':value);
+  const [show, setShow] = useState(false);
+  const [autoCompleteValue, setAutoCompleteValue] = useState((value === undefined) ? '' : value);
+  const [validationStatus, setValidationStatus] = useState({});
 
- const DataFilter = (val, status) =>{
-  setAutoCompleteValue(val);
-  setFilteredData(FilterArray(autoCompleteData, val));
-  setShow(status);
- };
+  const DataFilter = (val, status) => {
+    setAutoCompleteValue(val);
+    setFilteredData(FilterArray(autoCompleteData, val));
+    setShow(status);
+  };
 
- useEffect(()=>{
-  let formName = formContext?.name;
-  let form = formContext?.form;
-  formContext?.setForm(Object.assign(form, { 
-    [formName] : { 
-      [name]: autoCompleteValue,
-      "validationSteps": validationSteps,
-      "validationStatus":{
-
-      } }
+  useEffect(() => {
+    // validation
+    let result = {};
+    if (validation !== undefined) {
+      result = FormInputValidate(validation, autoCompleteValue);
+      console.log(result);
+      setValidationStatus(result);
+    }
+    // form Data
+    let formName = formContext?.name;
+    let form = formContext?.form;
+    formContext?.setForm(Object.assign(form, {
+      [formName]: {
+        [name]: {
+          value: autoCompleteValue,
+          result: result
+        }
+      }
     }));
-  // validationSteps
- },[autoCompleteValue]);
+  }, [autoCompleteValue]);
 
- return (<>
- <label className="form-label"><b>{label} :</b></label>
- <div className="dropdown"
- tabIndex={1}
- onBlur={(e) => {
-   if (!e.currentTarget.contains(e.relatedTarget)) {
-    setShow(false);
-   }
- }}>
- <input type="type" name={name} className="form-control dropdown-toggle" 
-  placeholder={placeholder} data-bs-toggle="dropdown" aria-expanded="false" 
-  value={autoCompleteValue} 
- onChange={(e)=>{ DataFilter(e.target.value, true); }} 
- required={validation?.required?.value}
- />
- <div align="right" className="invalid-feedback">{validation?.required?.value}</div>
- {show && filteredData.length>0 && (
-  <ul className="dropdown-menu autocomplete-dropdown show" aria-labelledby={name}>
-  {filteredData.map((d, i)=>{
-    const htmlToReactParser = new HtmlToReactParser.Parser();
-    return (<li key={i} onClick={()=>{ DataFilter(d, false); }}>
-     <a className="dropdown-item">
-      { htmlToReactParser.parse( d.replace( new RegExp(autoCompleteValue, "gi"), (str)=>{return '<b>'+str+'</b>'} ) ) }</a>
-    </li>);
-  })}
- </ul>
- )}
- 
-</div>
-</>);
+  return (<>
+    <label className={(autoCompleteValue.length > 0 ?
+      ((validationStatus?.errorMessage?.length > 0) ? "form-label form-label-validation-invalid" :
+        "form-label form-label-validation-valid")
+      : "form-label")}><b>{label} :</b></label>
+    <div className="dropdown"
+      tabIndex={1}
+      onBlur={(e) => {
+        if (!e.currentTarget.contains(e.relatedTarget)) {
+          setShow(false);
+        }
+      }}>
+      <input type="type" name={name}
+        className={(autoCompleteValue.length > 0 ?
+          ((validationStatus?.errorMessage?.length > 0) ? "form-control dropdown-toggle form-control-validation-invalid" :
+            "form-control dropdown-toggle form-control-validation-valid")
+          : "form-control dropdown-toggle")}
+        placeholder={placeholder} data-bs-toggle="dropdown" aria-expanded="false"
+        value={autoCompleteValue}
+        onChange={(e) => { DataFilter(e.target.value, true); }}
+        onKeyPress={(e) => KeyPressValidate(e, validation?.pattern?.value)}
+      />
+
+      {(autoCompleteValue.length > 0 && validationStatus?.errorMessage?.length > 0) &&
+        <div align="right" className="form-feedback-validation-invalid">
+          {validationStatus?.errorMessage}</div>
+      }
+
+
+      {show && filteredData.length > 0 && (
+        <ul className="dropdown-menu autocomplete-dropdown show" aria-labelledby={name}>
+          {filteredData.map((d, i) => {
+            const htmlToReactParser = new HtmlToReactParser.Parser();
+            return (<li key={i} onClick={() => { DataFilter(d, false); }}>
+              <a className="dropdown-item">
+                {htmlToReactParser.parse(d.replace(new RegExp(autoCompleteValue, "gi"),
+                  (str) => { return '<b>' + str + '</b>' }))}</a>
+            </li>);
+          })}
+        </ul>
+      )}
+
+    </div>
+  </>);
 };
--- e-ui-r/src/Utils/Validations.js
+++ e-ui-r/src/Utils/Validations.js
@@ -1,31 +1,31 @@
 export const FormInputValidate = (validation, value) => {
     const validationSteps = Object.keys(validation);
-    console.log(validationSteps);
+    console.log("validationSteps",validationSteps);
     const validationSuccess = [];
     let result;
     for (let validateStep in validationSteps) {
       const step = validationSteps[validateStep];
       let errorMessage = '';
-      if (step === 'validate') {
+       if (step === 'validate') {
         const errorMessage = validation[step](value);
         if (errorMessage.length === 0) {
           validationSuccess.push(step);
         }
         result = { validationSuccess, value, errorMessage };
-      } else if(step !== 'deps') {
+      } 
+       else if(step === 'deps') {
 
       }
       else {
         if (
           (step === 'required' && value.trim().length === 0) ||
-          (step === 'minLength' &&
-            value.trim().length < validation[step]?.value) ||
+          (step === 'minLength' && value.trim().length < validation[step]?.value) ||
           (step === 'pattern' && !validation[step]?.value.test(value)) ||
-          (step === 'maxLength' &&
-            value.trim().length > validation[step]?.value)
+          (step === 'maxLength' && value.trim().length > validation[step]?.value)
         ) {
           errorMessage = validation[step]?.errorMessage;
           result = { validationSuccess, value, errorMessage };
+          console.log("errorMessage: ", errorMessage);
           break;
         } else {
           validationSuccess.push(step);
@@ -34,12 +34,15 @@ export const FormInputValidate = (validation, value) => {
       }
     }
     console.log('result', result);
+    return result;
 };
 
-export const KeyPressValidate=(event, pattern)=>{
-    let key = String.fromCharCode(event.which || event.charCode);
-    if (!pattern.test(key)) {
-     FormInputValidate(validation, event.target.value + key);
-     event.preventDefault();
-    }
+export const KeyPressValidate=(event, pattern, validation)=>{
+    if(pattern!==undefined){
+      let key = String.fromCharCode(event.which || event.charCode);
+      if (!pattern.test(key)) {
+       FormInputValidate(validation, event.target.value + key);
+       event.preventDefault();
+      }
+    }  
 };
