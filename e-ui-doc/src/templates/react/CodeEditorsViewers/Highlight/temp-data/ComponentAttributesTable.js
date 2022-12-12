d1be5ff735faf71d30f94db2eba89b90d5f717b0
AnupChakravarthi <nellutlalnrao@gmail.com>
2022-12-12 11:49:18 +0530

    Added Component Attributes to the Doc Library

--- e-ui-doc/src/templates/react/CodeEditorsViewers/Highlight/temp-data/ComponentAttributesTable.js
+++ e-ui-doc/src/templates/react/CodeEditorsViewers/Highlight/temp-data/ComponentAttributesTable.js
@@ -6,5 +6,17 @@ export const ComponentAttributesTable = {
         { "columnName": "Description", "id": "description", "dataAlign":"left", "width": "30%" },
         { "columnName": "Example", "id": "example", "dataAlign":"left", "width": "40%" }
     ],
-    "data": []
+    "data": [{
+        "attributeName": (<div>content <Badge type="success" label="Mandatory" size="11" /></div>),
+        "description": "It contains component or code that want to display as a highlight editor",
+        "example": (<div align="center"><b>-</b></div>)
+    },{
+        "attributeName": (<div>lang <Badge type="success" label="Mandatory" size="11" /></div>),
+        "description": "Programming language for which the content belongs to",
+        "example": (<div align="center"><b>-</b></div>)
+    },{
+        "attributeName": (<div>lines <Badge type="warning" label="Optional" size="11" /></div>),
+        "description": "The group of lines or a single line of a code that wants to be displayed as a highlight editor",
+        "example": (<div align="center"><b>-</b></div>)
+    }]
 };
