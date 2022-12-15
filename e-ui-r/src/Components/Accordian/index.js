7ef61dc52e67fe188e0b68684b9f3a0a02d300c7
AnupChakravarthi <nellutlalnrao@gmail.com>
2022-12-15 23:55:50 +0530

    Updated Accordian in the Library

--- e-ui-r/src/Components/Accordian/index.js
+++ e-ui-r/src/Components/Accordian/index.js
@@ -26,19 +26,21 @@ export const Accordian = ({ id, data, defaultOpen })=>{
  };
 
  return (<>
- <div className="accordion" id={id}>
+ <div className="list-group" id={id}>
  {data.map((d, i)=>{
     console.log(showAccordian, d.id);
-    let collapseDivClass = (showAccordian.includes(d.id))?"accordion-collapse collapse show":"accordion-collapse collapse ";
-    let collapseTitleClass = (showAccordian.includes(d.id))?"accordion-button":"accordion-button collapsed";
-    return (<div key={i} className="accordion-item">
-      <button  id={"heading-"+d.id} onClick={()=>toggleAccordian(d.id)} className={"accordion-header "+collapseTitleClass} type="button" data-bs-toggle="collapse" data-bs-target={"#"+d.id} aria-expanded="true" aria-controls={d.id}>
-       {d.title}
-      </button>
-    <div id={d.id} className={collapseDivClass} aria-labelledby={"heading-"+d.id} data-bs-parent={"#"+id}>
+    let collapseDivClass = (showAccordian.includes(d.id))?"collapse show":"collapse ";
+     return (<>
+     <div id={"heading-"+d.id} key={i} className="list-group-item" onClick={()=>toggleAccordian(d.id)} 
+     data-bs-toggle="collapse" data-bs-target={"#"+d.id} aria-expanded="true" aria-controls={d.id} 
+     style={{ cursor:'pointer' }}>
+      <span style={{ float:'left' }}>{d.title}</span>
+      <span style={{ float:'right' }}>R</span>
+    </div>
+    <div id={d.id} className={"list-group-item "+collapseDivClass} aria-labelledby={"heading-"+d.id} data-bs-parent={"#"+id}>
       <div className="accordion-body">{d.component}</div>
     </div>
-  </div>);
+  </>);
  })}
  </div>
  </>);
